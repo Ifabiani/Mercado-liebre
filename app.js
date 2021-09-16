3b5d00583545c6504196dddf3a924965efb990cb
Ifabiani <ignaciofabiani@gmail.com>
2021-09-16 18:29:28 -0300

    A corregir

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const morgan = require('morgan')
+
+//const publicPath = path.resolve(__dirname, './Public');
+
+app.listen(3000, ()=>console.log('Starting server 3000'));
+app.use(express.static(path.resolve(__dirname,'./Public')));
+app.get('/', function (req, res) {
+    res.sendFile(path.resolve(__dirname, './Views/home.html'))
+});
+
+app.get('/register.html', function (req, res) {
+    res.sendFile(path.resolve(__dirname, './Views/register.html'))
+});
+
+app.get('/login.html', function (req, res) {
+    res.sendFile(path.resolve(__dirname, './Views/login.html'))
+});
+
+
+app.use(morgan('tiny'))
